763d8375ac70f7c0f9bbfdab1e0a9826f2fd8f4d
Kacper Słomiński <kacper.slominski72@gmail.com>
2025-12-01 21:55:36 +0100

    Use spaces instead of tabs in Scheme

--- 2025/day1/solution.scm
+++ 2025/day1/solution.scm
@@ -2,16 +2,16 @@
 
 (define (parse-line line)
   (let ([sgn (string-take line 1)]
-	[val (string-drop line 1)])
+        [val (string-drop line 1)])
     (* (string->number val)
        (if (string=? sgn "L") -1 +1))))
 
 (define (read-input)
   (let ([line (get-line (current-input-port))])
     (if (eof-object? line)
-	'()
-	(cons (parse-line line)
-	      (read-input)))))
+        '()
+        (cons (parse-line line)
+              (read-input)))))
 
 (define (found-0s-part-1 cur val)
   (if (eq? cur 0) 1 0))
@@ -19,20 +19,20 @@
 (define (found-0s-part-2 cur val)
   (truncate
    (/ (if (< val 0)
-	  (- (euclidean-remainder (- cur) 100) val)
-	  (+ cur val))
+          (- (euclidean-remainder (- cur) 100) val)
+          (+ cur val))
     100)))
 
 (define (part-common input count-found-0s)
   (let next ([input input]
-	     [cur 50]
-	     [found-0s 0])
+             [cur 50]
+             [found-0s 0])
     (if (null? input)
-	found-0s
-	(next (cdr input)
-	      (euclidean-remainder (+ cur (car input)) 100)
-	      (+ found-0s
-		 (count-found-0s cur (car input)))))))
+        found-0s
+        (next (cdr input)
+              (euclidean-remainder (+ cur (car input)) 100)
+              (+ found-0s
+                 (count-found-0s cur (car input)))))))
 
 
 (let ([input (read-input)])
